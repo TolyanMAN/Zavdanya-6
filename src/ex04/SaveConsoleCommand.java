7a842a3a5bd3e656898f03bdcb721cab9771e3cd
Максим Герасименко <76556732+TolyanMAN@users.noreply.github.com>
2022-05-02 13:53:30 +0300

    Update SaveConsoleCommand.java

--- src/ex04/SaveConsoleCommand.java
+++ src/ex04/SaveConsoleCommand.java
@@ -4,20 +4,29 @@ import ex02.View;
 /** Консольная команда
  * Save;
  * шаблон Command
- * @author Vlada
+ 
+ * @author Maksim
+ 
  * @version 1.0
  */
 public class SaveConsoleCommand implements ConsoleCommand {
+    
 
     /** Объект, реализующий интерфейс {@linkplain View};
+    
      * обслуживает коллекцию объектов {@linkplain ex01.Item2d}
+     
      */
     public View view;
+    
 
     /** Инициализирует поле {@linkplain SaveConsoleCommand#view}
+    
      * @param view объект, реализующий интерфейс {@linkplain View}
+     
      */
     public SaveConsoleCommand(View view) {
+        
         this.view = view;
     }
 
@@ -27,15 +36,19 @@ public class SaveConsoleCommand implements ConsoleCommand {
     }
     @Override
     public String toString() {
+        
         return "'s'ave";
     }
     @Override
+    
     public void execute() {
         System.out.println("Save current.");
         try {
+            
             view.viewSave();
         } catch (IOException e) {
             System.err.println("Serialization error: " + e);
+            
         }
         view.viewShow();
     }
