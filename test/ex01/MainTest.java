072fbccc272e6ec2b93e002e2f9218d8a85e531f
Максим Герасименко <76556732+TolyanMAN@users.noreply.github.com>
2022-05-02 11:30:32 +0300

    Update MainTest.java

--- test/ex01/MainTest.java
+++ test/ex01/MainTest.java
@@ -10,26 +10,38 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * ��������� ������������ ������������� �������.
  *
- * @author Vlada
+ * @author Maksim
+ 
  * @version 1.0
  */
 @SuppressWarnings("SpellCheckingInspection")
+
 public class MainTest {
+    
     /**
      * �������� �������� ���������������� ������ {@linkplain Calc}
+     
      */
     @Test
+    
     public void testCalc() {
+        
         Calc calc = new Calc();
         calc.init(new double[]{0, 100, 200, 300});
+        
         assertEquals(4, calc.getResult().getOnesNumber());
+        
         calc.init(new double[]{25, 50, 75, 100});
         assertEquals(3, calc.getResult().getOnesNumber());
+        
         calc.init(new double[]{50, 100, 150, 200});
         assertEquals(5, calc.getResult().getOnesNumber());
         calc.init(new double[]{100, 120, 140, 160});
+        
         assertEquals(6, calc.getResult().getOnesNumber());
+        
         calc.init(new double[]{0, 50, 100, 200});
+        
         assertEquals(5, calc.getResult().getOnesNumber());
     }
 
@@ -40,10 +52,14 @@ public class MainTest {
     public void testRestore() {
         Calc calc = new Calc();
         int onesNumber;
+        
         double []arguments;
         for (int ctr = 0; ctr < 360; ctr++) {
+            
             arguments = new double[]{ctr, ctr, 360 - ctr, 360 - ctr};
+            
             onesNumber = calc.init(arguments);
+            
             try {
                 calc.save();
             } catch (IOException e) {
