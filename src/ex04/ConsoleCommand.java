57f1f4e24cbb3d1b053ffd8c6429950db4f79849
Максим Герасименко <76556732+TolyanMAN@users.noreply.github.com>
2022-05-02 13:50:33 +0300

    Update ConsoleCommand.java

--- src/ex04/ConsoleCommand.java
+++ src/ex04/ConsoleCommand.java
@@ -1,15 +1,25 @@
 package ex04;
 /** Интерфейс
+
  * консольной команды;
+ 
  * шаблон Command
- * @author Vlada
+ 
+ * @author Maksim
+ 
  * @version 1.0
  */
 public interface ConsoleCommand extends Command {
 
+    
     /** Горячая клавиша команды;
+    
      * шаблон Command
+     
      * @return символ горячей клавиши
      */
+    
     public char getKey();
+    
 }
+
