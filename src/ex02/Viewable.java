1c910b0a7ff8308a71efe2bc955dee1e0d228110
Максим Герасименко <76556732+TolyanMAN@users.noreply.github.com>
2022-05-02 13:43:16 +0300

    Update Viewable.java

--- src/ex02/Viewable.java
+++ src/ex02/Viewable.java
@@ -1,15 +1,21 @@
 package ex02;
 /** Creator
+
  * (шаблон проектирования
+ 
  * Factory Method)<br>
+ 
  * Объявляет метод,
+ 
  * "фабрикующий" объекты
- * @author Vlada
+ * @author Maksim
+ 
  * @version 1.0
  * @see Viewable#getView()
  */
 public interface Viewable {
     /** Создаёт объект, реализующий {@linkplain View} */
+    
     View getView();
 
 }
