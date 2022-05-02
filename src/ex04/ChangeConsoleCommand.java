f451582a9ea8bcf5b643b52e05e9e797c674db0b
Максим Герасименко <76556732+TolyanMAN@users.noreply.github.com>
2022-05-02 13:49:18 +0300

    Update ChangeConsoleCommand.java

--- src/ex04/ChangeConsoleCommand.java
+++ src/ex04/ChangeConsoleCommand.java
@@ -5,33 +5,47 @@ import ex02.ViewResult;
 /** Консольная команда
  * Change item;
  * шаблон Command
- * @author Vlada
+ * @author Maksim
+ 
  * @version 1.0
  */
 public class ChangeConsoleCommand
+        
         extends ChangeItemCommand
+        
         implements ConsoleCommand {
 
     /** Объект, реализующий интерфейс {@linkplain View};
+    
      * обслуживает коллекцию объектов {@linkplain ex01.Item2d}
+     
      */
     private View view;
 
+
     /** Возвращает поле {@linkplain ChangeConsoleCommand#view}
+    
      * @return значение {@linkplain ChangeConsoleCommand#view}
+     
      */
     public View getView() {
         return view;
     }
     /** Устанавливает поле {@linkplain ChangeConsoleCommand#view}
      * @param view значение для {@linkplain ChangeConsoleCommand#view}
+     
      * @return новое значение {@linkplain ChangeConsoleCommand#view}
      */
     public View setView(View view) {
         return this.view = view;
+        
     }
+    
+    
     /** Инициализирует поле {@linkplain ChangeConsoleCommand#view}
+    
      * @param view объект, реализующий интерфейс {@linkplain View}
+     
      */
     public ChangeConsoleCommand(View view) {
         this.view = view;
