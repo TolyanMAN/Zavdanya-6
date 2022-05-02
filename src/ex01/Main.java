275594388f9a18bbb1b265397534722d980938b2
Максим Герасименко <76556732+TolyanMAN@users.noreply.github.com>
2022-05-02 11:40:16 +0300

    Update Main.java

--- src/ex01/Main.java
+++ src/ex01/Main.java
@@ -8,16 +8,21 @@ import java.io.InputStreamReader;
  * Calculation and display of results.
  * Contains the implementation of the static main() method.
  *
- * @author Vlada
+ * @author Maksim
+ 
  * @version 1.0
  * @see Main#main
+ 
  */
 
 public class Main {
+    
     /**
      * class object {@linkplain Calc}.solves the task of an individual task
+     
      */
     private final Calc calc = new Calc();
+    
 
     /**
      * Displays the menu.
@@ -25,14 +30,20 @@ public class Main {
     private void menu() {
         String s = null;
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        
         do {
             do {
                 System.out.println("Enter command...");
+                
                 System.out.print("'q'uit, 'v'iew, 'g'enerate, 's'ave, 'r'estore: ");
                 try {
+                    
                     s = in.readLine();
+                    
                 } catch (IOException e) {
+                    
                     System.out.println("Error: " + e);
+                    
                     System.exit(0);
                 }
             } while (s.length() != 1);
@@ -57,10 +68,12 @@ public class Main {
                     calc.show();
                 }
                 case 'r' -> {
+                    
                     System.out.println("Restore last saved.");
                     try {
                         calc.restore();
                     } catch (Exception e) {
+                        
                         System.out.println("Serialization error: " + e);
                     }
                     calc.show();
@@ -72,7 +85,9 @@ public class Main {
 
     /**
      * Executed when the program starts.
+     
      * The value of the function is calculated for the various arguments.
+     
      * The calculation results are displayed on the screen.
      *
      * @param args - program launch options.
