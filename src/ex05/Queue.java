5936340555807b94467a0953758850f5cc2bc895
Максим Герасименко <76556732+TolyanMAN@users.noreply.github.com>
2022-05-02 14:03:54 +0300

    Update Queue.java

--- src/ex05/Queue.java
+++ src/ex05/Queue.java
@@ -2,16 +2,23 @@ package ex05;
 import ex04.Command;
 /** Представляет
  * методы для помещения
+ 
  * и извлечения задач
+ 
  * обработчиком потока;
+ 
  * шаблон Worker Thread
- * @author Vlada
+ 
+ * @author Maksim
+ 
  * @version 1.0
  * @see Command
  */
 public interface Queue {
     /** Добавляет новую задачу в очередь;
+    
      * шаблон Worker Thread
+     
      * @param cmd задача
      */
     void put(Command cmd);
