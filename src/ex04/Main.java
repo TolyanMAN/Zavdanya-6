13f64f993e24cceba70372b51370f2733f809c37
Максим Герасименко <76556732+TolyanMAN@users.noreply.github.com>
2022-05-02 13:52:13 +0300

    Update Main.java

--- src/ex04/Main.java
+++ src/ex04/Main.java
@@ -1,18 +1,28 @@
 package ex04;
 /** Вычисление и отображение
+
  * результатов; cодержит реализацию
+ 
  * статического метода main()
- * @author Vlada
+ 
+ * @author Maksim
+ 
  * @version 4.0
  * @see Main#main
  */
 public class Main {
+    
     /** Выполняется при запуске программы;
+    
      * вызывает метод {@linkplain Application#run()}
+     
      * @param args параметры запуска программы
+     
      */
     public static void main(String[] args) {
+        
         Application app = Application.getInstance();
+        
         app.run();
     }
 }
