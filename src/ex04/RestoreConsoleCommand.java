3fbbed895a1a3e09a03952e3276737ce472c39c0
Максим Герасименко <76556732+TolyanMAN@users.noreply.github.com>
2022-05-02 13:52:59 +0300

    Update RestoreConsoleCommand.java

--- src/ex04/RestoreConsoleCommand.java
+++ src/ex04/RestoreConsoleCommand.java
@@ -3,20 +3,28 @@ import ex02.View;
 /** Консольная команда
  * Restore;
  * шаблон Command
- * @author Vlada
+ * @author Maksim
+ 
  * @version 1.0
  */
 public class RestoreConsoleCommand implements ConsoleCommand {
+    
 
 /** Объект, реализующий интерфейс {@linkplain View};
+
  * обслуживает коллекцию объектов {@linkplain ex01.Item2d}
+ 
  */
     private View view;
+    
 
     /** Инициализирует поле {@linkplain RestoreConsoleCommand#view}
+    
      * @param view объект, реализующий интерфейс {@linkplain View}
+     
      */
     public RestoreConsoleCommand(View view) {
+        
         this.view = view;
     }
 
@@ -31,10 +39,15 @@ public class RestoreConsoleCommand implements ConsoleCommand {
     @Override
     public void execute() {
         System.out.println("Restore last saved.");
+        
         try {
+            
             view.viewRestore();
+            
         } catch (Exception e) {
+            
             System.err.println("Serialization error: " + e);
+            
         }
         view.viewShow();
     }
